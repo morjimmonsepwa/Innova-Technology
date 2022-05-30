8128ff92d8bf678c07e32dc48a69d250f39859ab
Jazmin Meza <jmeza@evolutionclub.com.mx>
2022-05-30 18:24:10 -0500

    Modificación del index de tickets.

--- resources/views/admin/pages/home/index.blade.php
+++ resources/views/admin/pages/home/index.blade.php
@@ -4,10 +4,10 @@
 
         <!-- Page Cabecera -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Pagina Principal</h1>
+        <h1 class="h3 mb-0 text-gray-800">Página Principal</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
             <iclass="fas fa-download fa-sm text-white-50"></i> 
-                Generate Report
+                Generar Reporte
         </a>
     </div>
 
@@ -34,11 +34,11 @@
 
     <!-- Earnings (Tickets Abiertos) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card border-left-success shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                             Tickets (Abiertos)</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">215,000</div>
                     </div>
@@ -57,7 +57,7 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                            Tickets en (Proceos)</div>
+                            Tickets (En Proceso)</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">215,000</div>
                     </div>
                     <div class="col-auto">
@@ -70,12 +70,12 @@
 
     <!-- Earnings (Tickets Abiertos) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
+        <div class="card border-left-danger shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                            Tickets (Abiertos)</div>
+                        <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                            Tickets (Cerrados)</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">215,000</div>
                     </div>
                     <div class="col-auto">
@@ -101,30 +101,30 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
+                            <th>Nombre</th>
+                            <th>Posición</th>
+                            <th>Oficina</th>
+                            <th>Año</th>
+                            <th>Status</th>
+                            <th>Salario</th>
                         </tr>
                     </thead>
                     <tfoot>
                         <tr>
-                            <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
+                            <th>Nombre</th>
+                            <th>Posición</th>
+                            <th>Oficina</th>
+                            <th>Año</th>
+                            <th>Status</th>
+                            <th>Salario</th>
                         </tr>
                     </tfoot>
                     <tbody>
                         <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
+                            <td>Monserrat Morales</td>
+                            <td>Arquitecto de Sistema</td>
+                            <td>México</td>
+                            <td>22</td>
                             <td>
                                 <a class="btn btn-danger">
                                     Cerrado
@@ -157,10 +157,10 @@
                             <td>$86,000</td>
                         </tr>
                         <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
+                            <td>Monserrat Morales</td>
+                            <td>Arquitecto de Sistema</td>
+                            <td>México</td>
+                            <td>22</td>
                             <td>
                                 <a class="btn btn-danger">
                                     Cerrado
@@ -181,10 +181,10 @@
                             <td>$320,800</td>
                         </tr>
                         <tr>
-                            <td>Ashton Cox</td>
-                            <td>Junior Technical Author</td>
-                            <td>San Francisco</td>
-                            <td>66</td>
+                            <td>Monserrat Morales</td>
+                            <td>Arquitecto de Sistema</td>
+                            <td>México</td>
+                            <td>22</td>
                             <td>
                                 <a class="btn btn-warning">
                                     Cerrado
@@ -193,10 +193,10 @@
                             <td>$86,000</td>
                         </tr>
                         <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
+                            <td>Monserrat Morales</td>
+                            <td>Arquitecto de Sistema</td>
+                            <td>México</td>
+                            <td>22</td>
                             <td>
                                 <a class="btn btn-danger">
                                     Cerrado
@@ -229,10 +229,10 @@
                             <td>$86,000</td>
                         </tr>
                         <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
+                            <td>Monserrat Morales</td>
+                            <td>Arquitecto de Sistema</td>
+                            <td>México</td>
+                            <td>22</td>
                             <td>
                                 <a class="btn btn-danger">
                                     Cerrado
