98343c8c8050944d3c315b50613efbd5cee41e39
AngelMontesR <angelmontes963@gmail.com>
2022-06-08 19:19:46 -0500

    Creación Migración Team-Group

--- resources/views/admin/pages/grupos/grupos.blade.php
+++ resources/views/admin/pages/grupos/grupos.blade.php
@@ -55,4 +55,46 @@
     </div>
     <!-- /.container-fluid -->
     
+
+    <!-- Modales -->
+        <!-- Modal Agregar -->
+        <div class="modal fade" id="agregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Agregar Usuario</h5>
+                    </div>
+                    <form action="{{ route('store.users')}}" method="POST">
+                        @csrf
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Nombre</label>
+                                <input type="text" class="form-control" aria-describedby="emailHelp"  id="name" name="name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Correo</label>
+                                <input type="text" class="form-control"  id="email" name="email" >
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                                <input type="password" class="form-control" id="passsword" name="passsword">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Rol</label>
+                                <select class="form-control" id="rol" name="rol" required>
+                                    <option>Seleccione una opción:</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    <!-- Modal Agregar -->
+<!-- Modales -->
+
+
 @endsection
