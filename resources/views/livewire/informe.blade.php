d42782b89373839de4c3f29558b7ec8ad2abc2b7
Monserrat <morjimmonse81@gmail.com>
2022-06-27 22:15:50 -0600

    Test Tickets  Se modifico el nombre de Tickets en el dashboard a Informe de Tickets.

--- resources/views/livewire/informe.blade.php
+++ resources/views/livewire/informe.blade.php
@@ -1,10 +1,10 @@
 <div>
     <!-- Page Heading -->
-    <h1 class="h3 mb-2 text-gray-800">Tickets</h1>
+    <h1 class="h3 mb-2 text-gray-800">Informe de Tickets</h1>
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Listado de Tickets</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Listado de Informe de Tickets</h6>
         </div>
         <div class="card-body" wire:ignore>
             <div class="table-responsive">
