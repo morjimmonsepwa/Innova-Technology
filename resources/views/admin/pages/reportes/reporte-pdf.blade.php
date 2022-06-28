ba9430246de07b9f8346ce744cbb72c3becc9a6e
jazminmeza <jazmi.yayas13@gmail.com>
2022-06-28 02:49:27 -0500

    Modificación  Se modifico el archivo reporte-pdf

--- resources/views/admin/pages/reportes/reporte-pdf.blade.php
+++ resources/views/admin/pages/reportes/reporte-pdf.blade.php
@@ -3,130 +3,98 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Reporte del Ticket</title>
+    <link href="
+https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css
+
+" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
   <body>
     <div class="container">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Reporte</h5>
+                <table border="0" class="table">
+                <tr>
+                <td><center><img src="{{ asset('libs/index/img/logo/logo2.jpeg')}}" style="max-width: 125px;"></center></td>
+                <td>
+                <p><center>REPORTE DEL TICKET: {{ $ticket->id }}</p> 
+                <p><center>Desarrollado por Innova Technology</p>    
+                <p>Universidad Tecnológica de Nezahualcóyotl, Cto. Rey Nezahualcóyotl, Benito Juárez, C.P: 57000, Cd. Nezahualcóyotl, México. <br>
+                        Tel: 55 5441 3182.
+                </tr>
+        </table>
             </div>
             <br>
             <br>
-            <br>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label class="form-label">Folio</label>
-                    <input type="text" class="form-control" aria-describedby="emailHelp" value ="{{$ticket->id}}" id="motivo" name="motivo" placeholder="Ingrese el motivo de la queja o devolución" disabled>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Asunto</label>
-                    <select class="form-control" id="asunto" name="asunto" required disabled>
-                        <option>Seleccione una opción: </option>
-                        @switch($ticket->affair)
-                            @case(1)
-                                <option selected value="1">Queja</option>
-                                <option value="2">Devolución</option>
-                                @break
-                            @case(2)
-                                <option value="1">Queja</option>
-                                <option selected value="2">Devolución</option>
-                                @break
-                        @endswitch
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Motivo</label>
-                    <input type="text" class="form-control" aria-describedby="emailHelp" value ="{{$ticket->reason}}" id="motivo" name="motivo" placeholder="Ingrese el motivo de la queja o devolución" disabled>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Cliente</label>
-                    <input type="text" class="form-control" value ="{{$ticket->client}}" id="cliente" name="cliente" placeholder="Ingrese el nombre del cliente" disabled>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Vía Queja</label>
-                    <select class="form-control" id="via" name="via" disabled>
-                        @switch($ticket->via)
-                            @case(1)
-                                <option selected value="1">Email</option>
-                                <option value="2">Llamada</option>
-                                @break
-                            @case(2)
-                                <option value="1">Email</option>
-                                <option selected value="2">Llamada</option>
-                                @break
-                        @endswitch
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Empresa</label>
-                    <select class="form-control" id="empresa" name="empresa" disabled>
-                        <option>Seleccione una opción: </option>
-                        @foreach ($empresas as $empresa)
-                            @if ($empresa->id == $ticket->id_business)
-                                <option selected value="{{$empresa->id}}">{{$empresa->name}}</option>
-                            @else
-                                <option value="{{$empresa->id}}">{{$empresa->name}}</option>
-                            @endif
-                        @endforeach
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Producto</label>
-                    <input type="text" class="form-control"  value="{{$ticket->product}}" id="producto" name="producto" placeholder="Ingrese el nombre del producto" disabled>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Encargado</label>
-                    <select class="form-control" id="encargado" name="encargado" disabled>
-                        <option>Seleccione una opción: </option>
-                        @foreach ($usuarios as $usuario)
-                            @if ($usuario->id == $ticket->id_manager)
-                                <option selected value="{{$usuario->id}}">{{$usuario->name}}</option>
-                            @else
-                                <option value="{{$usuario->id}}">{{$usuario->name}}</option>
-                            @endif
-                        @endforeach
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Asignado a</label>
-                    <select class="form-control" id="encargado" name="encargado" disabled>
-                        <option>Seleccione una opción: </option>
-                        @foreach ($usuarios as $usuario)
-                            @if ($usuario->id == $ticket->id_assigned)
-                                <option selected value="{{$usuario->id}}">{{$usuario->name}}</option>
-                            @else
-                                <option value="{{$usuario->id}}">{{$usuario->name}}</option>
-                            @endif
-                        @endforeach
-                    </select>
-                </div>
-                <div class="mb-3">
-                    @switch($ticket->status)
-                        @case(1)
-                        <div class="mb-3">
-                            <label class="form-label">Estatus</label>
-                            <input type="text" class="form-control text-success"  value="Abierto"  disabled>
-                        </div>
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+        <tr>
+
+
+            <td>Folio: {{$ticket->id}} <br></td>
+            <td>Asunto: 
+                @switch($ticket->affair)
+                    @case(1)
+                            Queja
                         @break
-                        @case(2)
-                        <div class="mb-3">
-                            <label class="form-label">Estatus</label>
-                            <input type="text" class="form-control text-warning"  value="Proceso"  disabled>
-                        </div>
+                    @case(2)
+                            Devolución
                         @break
-                        @case(3)
-                        <div class="mb-3">
-                            <label class="form-label">Estatus</label>
-                            <input type="text" class="form-control text-danger"  value="Cerrado"  disabled>
-                        </div>
+                @endswitch
+                
+                <br></td>
+            <tr><td>Motivo: {{$ticket->reason}}<br></td>
+            <td>Cliente: {{$ticket->client}}<br></td>
+            <tr><td>Vía Queja:
+                @switch($ticket->via)
+                    @case(1)
+                            Email
                         @break
-                    @endswitch
-                </div>
-            </div>
-        </div>    
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+                    @case(2)
+                            Llamada
+                        @break
+            @endswitch<br>
+            </td>
+            <td>Empresa: 
+                @foreach ($empresas as $empresa)
+                    @if ($empresa->id == $ticket->id_business)
+                       {{$empresa->name}}
+                   @endif
+                @endforeach
+                <br></td>
+            <tr><td>Producto: {{$ticket->product}}<br></td>
+            <td>Encargado:
+                @foreach ($usuarios as $usuario)
+                    @if ($usuario->id == $ticket->id_manager)
+                        {{$usuario->name}}
+                    @endif
+                @endforeach
+                <br></td>
+            <tr><td>Asignado a: 
+                @foreach ($usuarios as $usuario)
+                    @if ($usuario->id == $ticket->id_assigned)
+                            {{$usuario->name}}
+                    @endif
+                @endforeach
+                <br></td>
+            <td>Estatus: @switch($ticket->status)
+                @case(1)
+                    Abierto
+                @break
+                @case(2)
+                    Proceso
+                @break
+                @case(3)
+                    Cerrado
+                @break
+            @endswitch
+            <br></td>
+        </tr>
+        </thead>
+        </table>
+    <script src="
+https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js
+
+" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
-</html>
+</html> 
