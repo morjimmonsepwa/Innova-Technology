5ecbaa19a9f4d3e84b6dbe52cfcf2c653628dd45
AngelMontesR <angelmontes963@gmail.com>
2022-07-03 12:02:54 -0500

    Modificación  Se deshabilita boton olvide contraseña

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,11 +36,6 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Olvidé contraseña') }}
-                    </a>
-                @endif
                 <x-jet-button class="ml-4">
                     {{ __('Login') }}
                 </x-jet-button>
