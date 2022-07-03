b653ea9af85ecee6dfe21174248e43c8188f7bbb
AndreaSanchez07 <andywaylee@gmail.com>
2022-07-02 21:38:22 -0500

    Modificación de TicketExport en app/exports. Se modifico el archivo que exporta un excel.

--- app/Exports/TicketExport.php
+++ app/Exports/TicketExport.php
@@ -3,15 +3,40 @@
 namespace App\Exports;
 
 use App\Models\Ticket;
+use DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class TicketExport implements FromCollection
+
+class TicketExport implements FromCollection, WithHeadings,ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
     */
+    public function headings(): array
+    {
+        return [
+            'Folio',
+            'Asunto',
+            'Motivo',
+            'Cliente',
+            'Vía Queja',
+            'Empresa',
+            'Producto',
+            'Generado',
+            'Encargado',
+            'Asignado a',
+            'Estatus',
+            'Fecha Creación',
+            'Fecha Actualización',
+
+        ];
+    }
+    
     public function collection()
     {
         return Ticket::all();
+    
     }
 }
