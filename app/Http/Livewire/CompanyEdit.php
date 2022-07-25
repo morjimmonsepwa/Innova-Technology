34957a78b5e0f2286e3e5bebc2db773af910aeb8
AngelMontesR <angelmontes963@gmail.com>
2022-07-24 23:57:37 -0500

    Modificación  Se corrige edit empresas

--- app/Http/Livewire/CompanyEdit.php
+++ app/Http/Livewire/CompanyEdit.php
@@ -16,13 +16,22 @@ class CompanyEdit extends Component
 
 
     protected $rules = [
-        'name' => 'regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/'
+        'name' => 'regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/',
+        'name' => 'required'
     ];
          
     protected $messages = [
         'name.required' => 'El campo nombre es requerido.',
         'name.regex' => 'Ingresar solo letras.',
     ];
+
+
+    public function mount(){
+
+        $this->name = companies::findOrFail($this->id_company)->select('name')->get()[0]['name'];
+
+    }
+
 
     public function render()
     {
@@ -35,10 +44,8 @@ class CompanyEdit extends Component
 
         $new  = companies::find($this->id_company);
 
-        if($this->name != ''){
-            $validatedData = $this->validate();
-            $new->name=$this->name;
-        }
+        $validatedData = $this->validate();
+        $new->name=$this->name;
 
         $new->save();
 
