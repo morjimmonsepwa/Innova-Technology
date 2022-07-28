e0c90742291cc69120bc882a102650bf454ee0f9
AngelMontesR <angelmontes963@gmail.com>
2022-07-28 00:25:00 -0500

    Modificación  Se crea funcionalidad de buscador

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -4,36 +4,28 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Ticket;
+use App\Models\Company;
+use App\Models\User;
 
 class Search extends Component
 {
 
-    public $searchnum = 0;
-    public $resultado;
-
-
-
-    public function mount(){
-
-        if(is_integer($this->searchnum )){
-            $this->resultado = Ticket::where('id',$this->searchnum)->get();
-        }
-    
-        if(is_string($this->searchnum )){
-            $this->resultado = Ticket::where('reason',$this->searchnum)->get();
-        }
-        
-    }
-
-
+    public $searchnum;
 
     public function render()
     {
-        
-        $ticket = $this->resultado;
+                
+        $empresas = Company::all();
+        $ticket = Ticket::where('id',$this->searchnum)->get();
+        $users = User::all();
 
         $param['ticket'] =  $ticket;
+        $param['empresas'] = $empresas;
+        $param['usuarios'] = $users;
+
 
         return view('livewire.search',$param);
     }
+
+
 }
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,19 +1,13 @@
 <div>
     <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
         <div class="input-group">                      
-            <input id="something" class="form-control py-2  mr-1 pr-5" list="somethingelse" wire:model="searchnum" placeholder="Buscar...">
-            <datalist id="somethingelse">
-                @foreach ($ticket as $item)
-                    <option value="{{$item->reason}}">{{$item->id}}</option>
-                @endforeach
-            </datalist>
+            <input id="something" class="form-control py-2  mr-1 pr-5" wire:model="searchnum" type="text" id="menu1" data-toggle="dropdown" wire:model="searchnum" placeholder="Buscar...">
         </label>
             <div class="input-group-append">
                 <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <i class="fas fa-search fa-sm"></i>
                 </button>
             </div>
-            {{$searchnum}}
         </div>
     </form>
      <!-- Modal -->
@@ -24,77 +18,118 @@
                     <h5 class="modal-title" id="exampleModalLabel">Busqueda Ticket</h5>
                 </div>
                 <div class="modal-body">
-                    @foreach ($ticket as $item)
-                        <div class="mb-3">
-                            <label class="form-label">Asunto</label>
-                            <select class="form-control" id="asunto">
-                                <option value=""null"">Seleccione una opción: </option>
-                                @switch($item->affair)
-                                    @case(1)
-                                        <option selected value="1">Queja</option>
-                                        <option value="2">Devolución</option>
-                                        @break
-                                    @case(2)
-                                        <option value="1">Queja</option>
-                                        <option selected value="2">Devolución</option>
-                                        @break
-                                @endswitch
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Motivo</label>
-                            <input type="text" class="form-control" aria-describedby="emailHelp" value="{{$item->reason}}" id="motivo"  placeholder="Ingrese el motivo de la queja o devolución">
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Cliente</label>
-                            <input type="text" class="form-control" id="cliente" value="{{$item->client}}" placeholder="Ingrese el nombre del cliente">
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Vía Queja</label>
-                            <select class="form-control" id="via">
-                                @switch($item->via)
-                                    @case(1)
-                                        <option selected value="1">Email</option>
-                                        <option value="2">Llamada</option>
-                                        @break
-                                    @case(2)
-                                        <option value="1">Email</option>
-                                        <option selected value="2">Llamada</option>
-                                        @break
-                                @endswitch
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Empresa</label>
-                            <select class="form-control" id="empresa">
-                                <option value=""null"">Seleccione una opción: </option>
-                                {{-- @foreach ($empresas as $empresa)
+                    @if (count($ticket) == 0)
+                        <h5>No se encontró el Ticket</h5>
+                    @else
+                        @foreach ($ticket as $item)
+                            <div class="mb-3">
+                                <label class="form-label">Asunto:</label>
+                                    @switch($item->affair)
+                                        @case(1)
+                                            <label class="form-label">
+                                                <b>Queja</b>
+                                            </label>
+                                            @break
+                                        @case(2)
+                                            <label class="form-label">
+                                                <b>Devolución</b>
+                                            </label>
+                                            @break
+                                    @endswitch
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Motivo:</label>
+                                <label class="form-label">
+                                    <b>{{$item->reason}}</b>
+                                </label>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Cliente:</label>
+                                <label class="form-label">
+                                    <b>{{$item->client}}</b>
+                                </label>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Vía Queja:</label>
+                                    @switch($item->via)
+                                        @case(1)
+                                            <label class="form-label">
+                                                <b>Email</b>
+                                            </label>
+                                            @break
+                                        @case(2)
+                                            <label class="form-label">
+                                                <b>Llamada</b>
+                                            </label>
+                                            @break
+                                    @endswitch
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Empresa:</label>
+                                @foreach ($empresas as $empresa)
                                     @if ($empresa->id == $item->id_business)
-                                        <option selected value="{{$empresa->id}}">{{$empresa->name}}</option>
-                                    @else
-                                        <option value="{{$empresa->id}}">{{$empresa->name}}</option>
+                                        <b>{{$empresa->name}}</b>
                                     @endif
-                                @endforeach --}}
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Producto</label>
-                            <input type="text" class="form-control" id="producto" value="{{$item->product}}" placeholder="Ingrese el nombre del producto">
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Encargado</label>
-                            <select class="form-control" id="encargado">
-                                <option value=""null"">Seleccione una opción: </option>
-                                {{-- @foreach ($usuarios as $usuario)
+                                @endforeach
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Producto:</label>
+                                <label class="form-label">
+                                    <b>{{$item->product}}</b>
+                                </label>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Generado por:</label>
+                                @foreach ($usuarios as $usuario)
+                                    @if ($usuario->id == $item->id_generate)
+                                        <b>{{$usuario->name}}</b>
+                                    @endif
+                                @endforeach
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Encargado:</label>
+                                @foreach ($usuarios as $usuario)
                                     @if ($usuario->id == $item->id_manager)
-                                        <option selected value="{{$usuario->id}}">{{$usuario->name}}</option>
-                                    @else
-                                        <option value="{{$usuario->id}}">{{$usuario->name}}</option>
+                                        <b>{{$usuario->name}}</b>
                                     @endif
-                                @endforeach --}}
-                            </select>
-                        </div>
-                    @endforeach
+                                @endforeach
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Asignado a:</label>
+                                @foreach ($usuarios as $usuario)
+                                    @if ($usuario->id == $item->id_assigned)
+                                        <b>{{$usuario->name}}</b>
+                                    @endif
+                                @endforeach
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Estatus:</label>
+                                @switch($item->status)
+                                    @case(1)
+                                        <label class="form-label">
+                                            <b>Abierto</b>
+                                        </label>
+                                        @break
+                                    @case(2)
+                                        <label class="form-label">
+                                            <b>Proceso</b>
+                                        </label>
+                                        @break
+                                    @case(3)
+                                        <label class="form-label">
+                                            <b>Cerrado</b>
+                                        </label>
+                                        @break
+                                @endswitch
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Fecha Registro:</label>
+                                    <label class="form-label">
+                                        <b>{{$item->created_at->format('d-m-Y')}}</b>
+                                    </label>
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
